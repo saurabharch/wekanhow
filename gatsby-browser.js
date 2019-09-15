ff4ac0575a38422c267ae9d0cef0f743dc550b9b
saurabharch <saurabhkashyap0001@gmail.com>
2019-09-15 21:45:15 +0530

    update gatsby-browser and gatsby-node files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -22,38 +22,40 @@
 //
 //
 
-const React = require('react')
-const { BrowserRouter } = require('react-router-dom')
-const { Provider } = require('react-redux')
-const Promise  = require('promise-polyfill')
+const React = require("react");
+const { BrowserRouter } = require("react-router-dom");
+const { Provider } = require("react-redux");
+const Promise = require("promise-polyfill");
 
-const createStore = require('./src/createStore').default();
+const createStore = require("./src/createStore").default();
 
- exports.replaceRouterComponent = ({ history }) => {
-  const store = createStore()
+exports.replaceComponentRenderer = ({ history }) => {
+  const store = createStore();
 
-  if (typeof Raven !== 'undefined') {
-    Raven.config('https://844ee16b8acb45e0a4bba7022e80e547@sentry.io/1407848').install()
+  if (typeof Raven !== "undefined") {
+    Raven.config(
+      "https://844ee16b8acb45e0a4bba7022e80e547@sentry.io/1407848"
+    ).install();
   }
 
   // for our IE11 users
   if (!window.Promise) {
-    window.Promise = Promise
+    window.Promise = Promise;
   }
 
   const ConnectedRouterWrapper = ({ children }) => (
     <Provider store={store}>
       <BrowserRouter history={history}>{children}</BrowserRouter>
     </Provider>
-  )
+  );
 
-  return ConnectedRouterWrapper
-}
+  return ConnectedRouterWrapper;
+};
 
 exports.onRouteUpdate = function({ location }) {
   // Don't track while developing.
   if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
-    ga(`set`, `page`, (location || {}).pathname)
-    ga(`send`, `pageview`)
+    ga(`set`, `page`, (location || {}).pathname);
+    ga(`send`, `pageview`);
   }
-}
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,60 +1,61 @@
-const path = require('path')
-const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
-const xmldom = require('xmldom')
-const {syncToAlgolia} = require('./algoliasync')
+const path = require("path");
+const StaticSiteGeneratorPlugin = require("static-site-generator-webpack-plugin");
+const xmldom = require("xmldom");
+const { syncToAlgolia } = require("./algoliasync");
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-  const { createNodeField } = boundActionCreators
-  let slug
+  const { createNodeField } = boundActionCreators;
+  let slug;
   if (node.internal.type === `MarkdownRemark`) {
-    const fileNode = getNode(node.parent)
-    const parsedFilePath = path.parse(fileNode.relativePath)
-    const splittedDir = parsedFilePath.dir.split('/')
+    const fileNode = getNode(node.parent);
+    const parsedFilePath = path.parse(fileNode.relativePath);
+    const splittedDir = parsedFilePath.dir.split("/");
     // remove /backend /frontend /graphql
     if (splittedDir.length === 2) {
-      slug = `/${splittedDir[1]}/${parsedFilePath.name}/`
+      slug = `/${splittedDir[1]}/${parsedFilePath.name}/`;
     } else if (parsedFilePath.name !== `index` && parsedFilePath.dir !== ``) {
-      slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`
+      slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
     } else if (parsedFilePath.dir === ``) {
-      slug = `/${parsedFilePath.name}/`
+      slug = `/${parsedFilePath.name}/`;
     } else {
-      slug = `/${parsedFilePath.dir}/`
+      slug = `/${parsedFilePath.dir}/`;
     }
 
     // Add slug as a field on the node.
-    createNodeField({ node, fieldName: `slug`, fieldValue: slug })
+    createNodeField({ node, fieldName: `slug`, fieldValue: slug });
   }
-}
+};
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+  const { createPage } = boundActionCreators;
 
   return new Promise((resolve, reject) => {
-    const pages = []
-    const tutorials = path.resolve('src/templates/Tutorials.tsx')
+    const pages = [];
+    const tutorials = path.resolve("src/templates/Tutorials.tsx");
     // Query for all markdown "nodes" and for the slug we previously created.
     resolve(
       graphql(
-        `{
-          allMarkdownRemark {
-            edges {
-              node {
-                frontmatter {
-                  title
+        `
+          {
+            allMarkdownRemark {
+              edges {
+                node {
+                  frontmatter {
+                    title
+                  }
+                  fields {
+                    slug
+                  }
+                  excerpt(pruneLength: 1000)
                 }
-                fields {
-                  slug
-                }
-                excerpt(pruneLength: 1000)
               }
             }
           }
-        }
-      `
+        `
       ).then(result => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
 
         // Create blog posts pages.
@@ -63,34 +64,37 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             path: edge.node.fields.slug, // required
             component: tutorials,
             context: {
-              slug: edge.node.fields.slug,
-            },
-          })
-        })
+              slug: edge.node.fields.slug
+            }
+          });
+        });
 
-        if (process.env.NODE_ENV === 'production') {
+        if (process.env.NODE_ENV === "production") {
           try {
-            syncToAlgolia(result.data)
+            syncToAlgolia(result.data);
           } catch (e) {
-            console.log('Warning: Algolia Error')
+            console.log("Warning: Algolia Error");
           }
         }
 
-        return
+        return;
       })
-    )
-  })
-}
+    );
+  });
+};
 
-exports.modifyWebpackConfig = function modifyWebpackConfig({config, stage}) {
-  config.removeLoader('md')
-  config.loader('md', {
+exports.onCreateWebpackConfig = function onCreateWebpackConfig({
+  config,
+  stage
+}) {
+  config.removeLoader("md");
+  config.loader("md", {
     test: /\.md$/,
-    loader: 'babel-loader!reactdown/webpack'
-  })
+    loader: "babel-loader!reactdown/webpack"
+  });
 
-  if (stage === 'build-html') {
-    const pages = config._config.plugins[0].paths
+  if (stage === "build-html") {
+    const pages = config._config.plugins[0].paths;
     config._config.plugins[0] = new StaticSiteGeneratorPlugin({
       entry: `render-page.js`,
       paths: pages,
@@ -98,18 +102,18 @@ exports.modifyWebpackConfig = function modifyWebpackConfig({config, stage}) {
         DOMParser: xmldom.DOMParser,
         NodeList: xmldom.NodeList,
         atob: function atob(str) {
-          return new Buffer(str, 'base64').toString('binary');
+          return new Buffer(str, "base64").toString("binary");
         },
         location: {
-          pathname: ''
+          pathname: ""
         },
         document: {
           addEventListener: () => null,
           removeEventListener: () => null
         }
       }
-    })
+    });
   }
 
-  return config
-}
+  return config;
+};
